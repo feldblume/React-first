b49ec5c91e940994811e7e2e8051aadc73f43035
Nataliia Kushnirenko <Nataliia.Kushnirenko@vistajet.com>
2016-11-15 10:06:05 +0200

    React app is up and running

---
+++ app/components/App.js
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var App = React.createClass({
+  render: function () {
+    return (
+	  <button>
+        Stop!
+      </button>
+    );
+  }
+});
+
+
+module.exports = App;
+
+
